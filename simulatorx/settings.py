5a6f50653661040eb7ff5ed8c1cb4998053363cc
terry-brett <85226033+terry-brett@users.noreply.github.com>
2022-03-24 12:51:32 +0100

    Separate form from views

--- simulatorx/settings.py
+++ simulatorx/settings.py
@@ -130,6 +130,7 @@ if DEBUG:
     )
     STATICFILES_DIRS = (
         os.path.join(os.path.dirname(BASE_DIR), "SimulatorX", "static", "static"),
+        os.path.join(os.path.dirname(BASE_DIR), "SimulatorX", "Templates", "includes", "static"),
     )
 
 # Static files (CSS, JavaScript, Images)
@@ -149,4 +150,5 @@ STATIC_ROOT = os.path.join(
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "SimulatorX", "static", "media")
 STATICFILES_DIRS = (
     os.path.join(os.path.dirname(BASE_DIR), "SimulatorX", "static", "static"),
+    os.path.join(os.path.dirname(BASE_DIR), "SimulatorX", "Templates", "includes", "static"),
 )
